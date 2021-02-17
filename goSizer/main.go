f10391ee42ba8b5e36736309f5769599bd1be330
matzi278 <64423424+matzi278@users.noreply.github.com>
2021-02-17 17:12:04 +0100

    Add files via upload

---
+++ goSizer/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) == 2 {
+		size := getDirSize(os.Args[1])
+		fmt.Printf("the size is:  %.3f mb", size)
+	} else {
+		println("only add one argument which has to be the path of the item to scan. Try enclousing the path in quotation marks")
+	}
+}
+
+func getDirSize(path string) float64 {
+	var size int64 = 0
+
+	readPath := func(path string, file os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !file.IsDir() {
+			size += file.Size()
+		}
+		if file.Size() == 0 {
+			println("path not found")
+		}
+		return err
+	}
+
+	filepath.Walk(path, readPath)
+	finalSize := float64(size) / 1024.0 / 1024.0
+	return finalSize
+}
